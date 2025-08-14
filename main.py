22d1ccc815d271abcb94c1d92485cc3758f0ae08
sbuuby <jakubt200@gmail.com>
2025-08-14 03:09:57 +0200

    implement pygame groups to streamline updating and drawing

--- main.py
+++ main.py
@@ -7,12 +7,19 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
     dt = 0
+    Player.containers = (updatable, drawable)
+    player = Player( SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    player = Player( SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+
+    
     
     while True:
         for event in pygame.event.get():
@@ -21,14 +28,12 @@ def main():
 
         dt = clock.tick(60)/1000
         
-        player.update(dt)
+        updatable.update(dt)
 
         screen.fill("black")
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
-
-        
-
 
 if __name__ == "__main__":
     main()
